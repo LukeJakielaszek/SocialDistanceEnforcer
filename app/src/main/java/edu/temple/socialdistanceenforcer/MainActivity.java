f5901fb061ba0e14b7fcdc3b606d5c7730103536
Luke Jakielaszek <tug52339@temple.edu>
2020-04-21 12:37:19 -0400

    Inifinitely cycled bluetooth discovery. Also added a timeout for the discovery for every 10 seconds. This allows us to start a new discovery to find nearby devices (even if we have already discovered a device previously). Acquired dataset by sampling at 1, 5, 10, 15, 20, and 25 feet.

--- app/src/main/java/edu/temple/socialdistanceenforcer/MainActivity.java
+++ app/src/main/java/edu/temple/socialdistanceenforcer/MainActivity.java
@@ -21,6 +21,9 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.nio.charset.MalformedInputException;
 import java.util.Set;
 
@@ -34,32 +37,6 @@ public class MainActivity extends AppCompatActivity {
     private String toastText = "";
     private BluetoothDevice remoteDevice;
     private static final String DEBUG_TAG = "BLUETOOTH_APP";
-
-    BroadcastReceiver bluetoothStateReceiver = new BroadcastReceiver() {
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            String prevStateExtra = BluetoothAdapter.EXTRA_PREVIOUS_STATE;
-            String curStateExtra = BluetoothAdapter.EXTRA_STATE;
-            int state = intent.getIntExtra(curStateExtra, -1);
-            int prevState = intent.getIntExtra(prevStateExtra, -1);
-
-            if(state == BluetoothAdapter.STATE_TURNING_ON){
-                toastText = "Bluetooth is turning on";
-                Toast.makeText(MainActivity.this, toastText, Toast.LENGTH_SHORT).show();
-            }else if(state == BluetoothAdapter.STATE_ON) {
-                toastText = "Bluetooth is on";
-                displayUI();
-                Toast.makeText(MainActivity.this, toastText, Toast.LENGTH_SHORT).show();
-            }else if(state == BluetoothAdapter.STATE_TURNING_OFF) {
-                toastText = "Bluetooth is turning off";
-                Toast.makeText(MainActivity.this, toastText, Toast.LENGTH_SHORT).show();
-            }else if(state == BluetoothAdapter.STATE_OFF){
-                toastText = "Bluetooth is off";
-                displayUI();
-                Toast.makeText(MainActivity.this, toastText, Toast.LENGTH_SHORT).show();
-            }
-        }
-    };
 
     protected void enableBluetooth(){
         bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
@@ -77,7 +54,6 @@ public class MainActivity extends AppCompatActivity {
 
                 this.connectButton.setVisibility(View.INVISIBLE);
                 this.disconnectButton.setVisibility(View.VISIBLE);
-
             }else{
                 // bluetooth is not enabled
                 statusText.setText("Bluetooth is disabled");
@@ -101,30 +77,17 @@ public class MainActivity extends AppCompatActivity {
         this.statusText = findViewById(R.id.statusText);
 
         Log.d(DEBUG_TAG, "Registering discovery receiver");
-        registerReceiver(discoveryResultReceiver, new IntentFilter(BluetoothDevice.ACTION_FOUND));
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction(BluetoothDevice.ACTION_FOUND);
+        intentFilter.addAction(BluetoothAdapter.ACTION_DISCOVERY_FINISHED);
+        registerReceiver(discoveryResultReceiver, intentFilter);
 
         this.displayUI();
 
         connectButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //String actionStateChanged = BluetoothAdapter.ACTION_STATE_CHANGED;
-                //String actionRequestEnable = BluetoothAdapter.ACTION_REQUEST_ENABLE;
-                //IntentFilter intentFilter = new IntentFilter(actionStateChanged);
-                //registerReceiver(bluetoothStateReceiver, intentFilter);
-                //startActivityForResult(new Intent(actionRequestEnable), 0);
-                String scanModeChanged = BluetoothAdapter.ACTION_SCAN_MODE_CHANGED;
-                String beDiscoverable = BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE;
-                IntentFilter intentFilter = new IntentFilter(scanModeChanged);
-                registerReceiver(discoveryResultReceiver, intentFilter);
-                startActivityForResult(new Intent(beDiscoverable), DISCOVERY_REQUEST);
-                // Begin discovery process, or request necessary permission
-                if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED)
-                    Log.d(DEBUG_TAG, "Valid Perms");
-                else {
-                    Log.d(DEBUG_TAG, "REQUEST PERM");
-                    ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, REQUEST_PERMISSION);
-                }
+                enableDiscoverability();
             }
         });
 
@@ -135,6 +98,23 @@ public class MainActivity extends AppCompatActivity {
                 displayUI();
             }
         });
+    }
+
+    protected void enableDiscoverability(){
+        String scanModeChanged = BluetoothAdapter.ACTION_SCAN_MODE_CHANGED;
+        String beDiscoverable = BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE;
+        IntentFilter intentFilter = new IntentFilter(scanModeChanged);
+        registerReceiver(discoveryResultReceiver, intentFilter);
+        Intent intent = new Intent(beDiscoverable);
+        intent.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 10);
+        startActivityForResult(intent, DISCOVERY_REQUEST);
+        // Begin discovery process, or request necessary permission
+        if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED)
+            Log.d(DEBUG_TAG, "Valid Perms");
+        else {
+            Log.d(DEBUG_TAG, "REQUEST PERM");
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, REQUEST_PERMISSION);
+        }
     }
 
     @Override
@@ -161,11 +141,11 @@ public class MainActivity extends AppCompatActivity {
                     toastText = "Found device: " + pairedDevice.getName() + "|" + lastUsedRemoteDevice;
                     Toast.makeText(MainActivity.this, toastText, Toast.LENGTH_SHORT).show();
                     remoteDevice = pairedDevice;
+
+
                 }
             }
-        }
-
-        if(remoteDevice == null){
+        }else{
             // no known paired devices found
             toastText = "Starting discovery for remote devices...";
             Toast.makeText(MainActivity.this, toastText, Toast.LENGTH_SHORT).show();
@@ -174,22 +154,65 @@ public class MainActivity extends AppCompatActivity {
             if(bluetoothAdapter.startDiscovery()) {
                 toastText = "Discovery thread started. Scanning for devices...";
                 Toast.makeText(MainActivity.this, toastText, Toast.LENGTH_SHORT).show();
+
             }else{
                 Toast.makeText(MainActivity.this, "Unable to scan for bluetooth devices", Toast.LENGTH_SHORT).show();
             }
         }
     }
 
-    BroadcastReceiver discoveryResultReceiver = new BroadcastReceiver() {
+    final BroadcastReceiver discoveryResultReceiver = new BroadcastReceiver() {
         @Override
-        public void onReceive(Context context, Intent intent) {
-            if (BluetoothDevice.ACTION_FOUND.equals(intent.getAction())) {
-                Log.d(DEBUG_TAG, "OnReceive");
+        public void onReceive(Context context, final Intent intent) {
+            String action = intent.getAction();
+            Log.d(DEBUG_TAG, action);
+
+            Log.d(DEBUG_TAG, "OnReceive");
+            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
                 remoteDevice = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
-                toastText = "Discovered: " + remoteDevice.getName() + ":" + remoteDevice.getAddress();
+                toastText = "Discovered: " + remoteDevice.getName() + ":" + remoteDevice.getAddress()
+                        + ":" + Short.toString(intent.getShortExtra(BluetoothDevice.EXTRA_RSSI, Short.MIN_VALUE));
                 Toast.makeText(MainActivity.this, toastText, Toast.LENGTH_SHORT).show();
                 Log.d(DEBUG_TAG, toastText);
+                final String file_data = remoteDevice.getName() + "|" + Short.toString(intent.getShortExtra(BluetoothDevice.EXTRA_RSSI, Short.MIN_VALUE)) + "\n";
+                final String dataFile = remoteDevice.getName() + "_class_" + remoteDevice.getBluetoothClass().getDeviceClass() + "_major_"
+                        + remoteDevice.getBluetoothClass().getMajorDeviceClass() + "_15_foot.txt";
+                Log.d(DEBUG_TAG, dataFile);
+
+                new Thread() {
+                    @Override
+                    public void run() {
+                        super.run();
+                        FileOutputStream fileOutputStream = null;
+                        try {
+                            File file = new File(MainActivity.this.getExternalFilesDir(null), dataFile);
+                            Log.d(DEBUG_TAG, file.getAbsolutePath());
+                            String outPath = file.getAbsolutePath();
+                            file.createNewFile();
+                            fileOutputStream = new FileOutputStream(file, true);
+
+                            fileOutputStream.write(file_data.getBytes(), 0, file_data.length());
+
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }finally {
+                            try {
+                                // close stream
+                                if(fileOutputStream != null){
+                                    fileOutputStream.close();
+                                }
+                            } catch (IOException ignored) {
+                            }
+                        }
+
+                    }
+                }.start();
+            } else if (BluetoothAdapter.ACTION_DISCOVERY_FINISHED.equals(action)) {
+                Log.d(DEBUG_TAG, "Discovery finished, relaunching");
+                //enableDiscoverability();
+                findDevices();
             }
+
         }
     };
 
@@ -205,7 +228,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        //unregisterReceiver(bluetoothStateReceiver);
         unregisterReceiver(discoveryResultReceiver);
     }
 }
